4ea758da988afdc04896016fa323bfcc89ccb2b0
diegoEmanuel77 <diegoo7u7.97@gmail.com>
2026-02-22 01:06:08 -0400

    ajuste

--- public/plantillas_correos/plantilla-1-1771641929.blade.php
+++
@@ -1,9 +0,0 @@
-<h5>solo prueba de email</h5>
-
-
-<div class="row">
-                            <div class="col-md-6">
-  
-  </div>
-                            <div class="col-md-6"></div>
- </div>
